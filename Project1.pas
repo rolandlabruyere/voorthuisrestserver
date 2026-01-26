3708cfa091926eab8eb20d989525c673bb810654
Roland Labruyere <roland.labruyere@gmail.com>
2026-01-26 16:55:07 +0100

    remove temp files

--- Project1.pas
+++ Project1.pas
@@ -10,7 +10,13 @@ uses
   FormUnit1 in 'FormUnit1.pas' {Form1},
   ServerMethodsUnit1 in 'ServerMethodsUnit1.pas',
   WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule},
-  ServerFunctionUnit in 'ServerFunctionUnit.pas';
+  ServerFunctionUnit in 'ServerFunctionUnit.pas',
+  CommonFunctionUnit in 'OverlayUnits\CommonFunctionUnit.pas',
+  CommonProcedureUnit in 'OverlayUnits\CommonProcedureUnit.pas',
+  JasonConstructorUnit in 'OverlayUnits\JasonConstructorUnit.pas',
+  JasonReaderUnit in 'OverlayUnits\JasonReaderUnit.pas',
+  RestUnit in 'OverlayUnits\RestUnit.pas',
+  FindFiles in 'OverlayUnits\FindFiles.pas';
 
 {$R *.res}
 
